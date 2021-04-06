bb0f662756af200f6b3d6385a2ca299786a48eef
BLJett <65201151+BLJett@users.noreply.github.com>
2021-04-05 20:16:15 -0500

    Hopefully this is working code

---
+++ ProgrammingAssignment3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Loop through the array of nodes, update the lowestTotal to hold
+// the lowest cost node so far until we find the lowest cost open
+// node in the array of nodes.
+// Params: none
+// Return: int, element of lowest  cost node with lowest index
+int findLowestOpenNode()
+{
+	int lowestOpenNode = 0;
+	int lowestTotal = 0;
+	return lowestOpenNode;
+}
+
+// Calculates distance between two given nodes
+// Params: none
+// Return: int, distance between the two nodes
+int calculateDistanceBetweenNodes()
+{
+	int distance = 0;
+	return distance;
+}
+
+// Finds and returns a list of all outgoing connections
+// from the given node.
+// Params: none
+// Return: ???
+int getConnections() // probably doesnt return an int, adjust as necessary
+{
+
+}
+
+// Find path from start to end node by initializing a start node, finding
+// all outgoing connections, picking the lowest outgoing connection and taking it.
+// We return an updated version of the graph 
+// Params: none
+// Return: object???
+int findPath()
+{
+
+}
+
+int main() 
+{
+
+}
